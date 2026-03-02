1f6c4374ba5b6befd39f33ab7ed8b4eb9894b877
CicilyVinod07 <cv8748@srmist.edu.in>
2026-03-02 13:44:07 +0530

    UC7: Introduced CharacterPattern class to encapsulate character data and banner patterns; implemented map-based retrieval to display OOPS

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,75 +1,89 @@
-public class OopsBannerApp {
+import java.util.HashMap;
+import java.util.Map;
+
+class CharacterPattern {
+
+    private char character;
+    private String[] pattern;
+
+    public CharacterPattern(char character, String[] pattern) {
+        this.character = character;
+        this.pattern = pattern;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public String[] getPattern() {
+        return pattern;
+    }
+}
+
+public class OOPSBannerApp {
+
+    private Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+    public OOPSBannerApp() {
+        initializePatterns();
+    }
 
     public static void main(String[] args) {
+        OOPSBannerApp app = new OOPSBannerApp();
+        app.displayWord("OOPS");
+    }
 
-        System.out.println(String.join("",
-                " *****  ",   // O
-                "   ",
-                " *****  ",   // O
-                "   ",
-                " ****** ",   // P
-                "   ",
-                " *****  "    // S
-        ));
+    // Initialize stored character-pattern mappings
+    private void initializePatterns() {
 
-        System.out.println(String.join("",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*     * "
-        ));
+        patternMap.put('O', new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        }));
 
-        System.out.println(String.join("",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*       "
-        ));
+        patternMap.put('P', new CharacterPattern('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        }));
 
-        System.out.println(String.join("",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "******  ",
-                "   ",
-                " *****  "
-        ));
+        patternMap.put('S', new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        }));
+    }
 
-        System.out.println(String.join("",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*       ",
-                "   ",
-                "      * "
-        ));
+    // Display any word using stored mappings
+    public void displayWord(String word) {
 
-        System.out.println(String.join("",
-                "*     * ",
-                "   ",
-                "*     * ",
-                "   ",
-                "*       ",
-                "   ",
-                "*     * "
-        ));
+        int height = 7;
 
-        System.out.println(String.join("",
-                " *****  ",
-                "   ",
-                " *****  ",
-                "   ",
-                "*       ",
-                "   ",
-                " *****  "
-        ));
+        for (int row = 0; row < height; row++) {
+
+            for (char ch : word.toCharArray()) {
+
+                CharacterPattern cp = patternMap.get(ch);
+
+                if (cp != null) {
+                    System.out.print(cp.getPattern()[row] + "   ");
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
